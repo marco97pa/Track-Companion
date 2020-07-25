10f6ba28578c037ef7a0b2ead2cb476ea4fcca52
marco97pa <marco97pa@live.it>
2020-07-25 20:18:53 +0200

    Authentication using API
    Authentication using API instead of WebView.
    A new launcher activity called "AuthActivity" added.

---
+++ app/src/main/java/com/marco97pa/trackmania/AuthActivity.java
@@ -0,0 +1,179 @@
+package com.marco97pa.trackmania;
+
+import androidx.appcompat.app.AppCompatActivity;
+import okhttp3.Call;
+import okhttp3.Credentials;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.crashlytics.FirebaseCrashlytics;
+import com.marco97pa.trackmania.player.Player;
+import com.marco97pa.trackmania.player.PlayerFragment;
+import com.marco97pa.trackmania.utils.FLog;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class AuthActivity extends AppCompatActivity {
+
+    private TextView emailTextView, passwordTextView;
+    private Button logInButton;
+    private ProgressBar loading;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+
+        emailTextView = (TextView) findViewById(R.id.username);
+        passwordTextView = (TextView) findViewById(R.id.password);
+        logInButton = (Button) findViewById(R.id.login);
+        loading = (ProgressBar) findViewById(R.id.loading);
+
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailTextView.getText().toString();
+                String password = passwordTextView.getText().toString();
+                String login = email + ":" + password;
+                byte[] data = login.getBytes(StandardCharsets.UTF_8);
+                String base64 = Base64.encodeToString(data, Base64.DEFAULT);
+                //TODO: Remove base64
+                //TODO: check nulls or empty
+                new AuthenticationTask().execute(email, password);
+            }
+        });
+    }
+
+    public class AuthenticationTask extends AsyncTask<String, Void, Auth> {
+
+        private static final String LOG_TAG = "AuthenticationTask";
+        private FLog log = new FLog(LOG_TAG);
+
+        private static final int RESPONSE_OK = 200;
+        private String ticket = null;
+        private String refreshToken = null;
+        private String accessToken = null;
+
+        protected Auth doInBackground(String... params) {
+            log.d( "Starting task...");
+            String username = params[0];
+            String password = params[1];
+            Auth auth = null;
+
+            /*
+             * Stage ZERO: GET A TICKET
+             * HTTP POST to https://public-ubiservices.ubi.com/v3/profiles/sessionsHeaders needed:
+             * Authorization = Basic <Base64 traditional Basic authentication of email and password>
+             *                 see https://en.wikipedia.org/wiki/Basic_access_authentication for more
+             * Ubi-AppId = internal Ubisoft game app id, got from parameters.xml
+             * Ubi-RequestedPlatformType = platform that requires data, got from parameters.xml
+             * Content-Type = application/json
+             */
+            log.d("Starting STAGE 0");
+
+            OkHttpClient client = new OkHttpClient();
+            try {
+                Request request = new Request.Builder()
+                        .url(getString(R.string.URL_stage0))
+                        .addHeader("Authorization", Credentials.basic(username, password))
+                        .addHeader("Ubi-AppId", getString(R.string.Ubi_AppId))
+                        .addHeader("Ubi-RequestedPlatformType", getString(R.string.Ubi_RequestedPlatformType))
+                        .addHeader("Content-Type", "application/json")
+                        .post(RequestBody.create("", null))
+                        .build();
+                Call call = client.newCall(request);
+                Response response = call.execute();
+                if(response.code() == RESPONSE_OK){
+                    String jsonData = response.body().string();
+                    JSONObject Jobject = new JSONObject(jsonData);
+                    ticket = Jobject.get("ticket").toString();
+                    log.d("Ticket: " + ticket);
+                }
+                else{
+                    log.d("Response: " + response.code());
+                }
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
+
+            if(ticket != null){
+
+                /*
+                 * Stage ONE: GET A TOKEN
+                 * HTTP POST to https://prod.trackmania.core.nadeo.online/v2/authentication/token/ubiservices needed:
+                 * Authorization = <ticket from Stage ZERO>
+                 *
+                 * Returns an accessToken value and a refreshToken value
+                 */
+                log.d("Starting STAGE 1");
+
+                try {
+                    Request request = new Request.Builder()
+                            .url(getString(R.string.URL_stage1))
+                            .addHeader("Authorization", "ubi_v1 t=" + ticket)
+                            .post(RequestBody.create("", null))
+                            .build();
+                    Call call = client.newCall(request);
+                    Response response = call.execute();
+                    if(response.code() == RESPONSE_OK){
+                        String jsonData = response.body().string();
+                        JSONObject Jobject = new JSONObject(jsonData);
+                        accessToken = Jobject.get("accessToken").toString();
+                        log.d("accessToken: " + accessToken);
+                        refreshToken = Jobject.get("refreshToken").toString();
+                        log.d("refreshToken: " + refreshToken);
+
+                        auth = new Auth(accessToken, refreshToken);
+                    }
+                } catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            return auth;
+        }
+
+        protected void onPostExecute(Auth auth) {
+            super.onPostExecute(auth);
+            //this method will be running on UI thread
+            loading.setVisibility(View.GONE);
+            if(auth != null) {
+                Snackbar.make(logInButton, getString(R.string.log_in_success), BaseTransientBottomBar.LENGTH_LONG).show();
+                //Launch MAIN ACTIVITY PASSING AUTH
+            }
+            else{
+                Snackbar.make(logInButton, getString(R.string.no_network), BaseTransientBottomBar.LENGTH_LONG).show();
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            loading.setVisibility(View.VISIBLE);
+            loading.setIndeterminate(true);
+        }
+    }
+
+}
